4b4d5b6a46bc2c1d385e33760c7de40a02835d44
Dmitry <dmitronus2015@gmail.com>
2024-11-17 20:24:09 +0200

    add product slider

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ $(function () {
     vertical: true,
   });
 });
+
+$(".product__name").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  focusOnSelect: true,
+  initialSlide: 4,
+  asNavFor: ".product__content",
+  vertical: true,
+  prevArrow:
+    '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+  nextArrow:
+    '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+});
+$(".product__content").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: ".product__name",
+  fade: true,
+  arrows: false,
+});
