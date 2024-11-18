eb5962e861cbedabe6ff756836bafa9037bc5b86
Dmitry <dmitronus2015@gmail.com>
2024-11-18 18:24:44 +0200

    add header adaptive width

--- js/main.js
+++ js/main.js
@@ -7,23 +7,29 @@ $(function () {
       '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
     vertical: true,
   });
-});
 
-$(".product__name").slick({
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  focusOnSelect: true,
-  asNavFor: ".product__content",
-  vertical: true,
-  prevArrow:
-    '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
-  nextArrow:
-    '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
-});
-$(".product__content").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: ".product__name",
-  vertical: true,
-  arrows: false,
+  $(".product__name").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: ".product__content",
+    vertical: true,
+    prevArrow:
+      '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+  });
+
+  $(".product__content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".product__name",
+    vertical: true,
+    arrows: false,
+  });
+
+  $(".menu__btn").on("click", function () {
+    $(".menu__btn").toggleClass("menu__btn--active");
+    $(".menu__list").toggleClass("menu__list--active");
+  });
 });
