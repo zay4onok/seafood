e32e2c00d6114695f635fb92d91379ca43fd3bbf
zay4onok <dmitronus2015@gmail.com>
2024-11-18 22:36:14 +0200

    add product and advantages adaptive

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ $(function () {
       '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          arrows: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 460,
+        settings: {
+          vertical: false,
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
   });
 
   $(".product__content").slick({
@@ -26,6 +46,14 @@ $(function () {
     asNavFor: ".product__name",
     vertical: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          vertical: false,
+        },
+      },
+    ],
   });
 
   $(".menu__btn").on("click", function () {
